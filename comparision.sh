b85b10e792ef67d61b2b65ce38c4cf2c4402c556
H-strangeone <hiteshgitman@gmail.com>
2024-01-13 23:38:44 -0800

    nahi chala abhi bhi

--- comparision.sh
+++ comparision.sh
@@ -7,19 +7,19 @@
 #  -o flag for or
 read -p"enter x" x
 read -p"enter y" y
-if [$x > $y]
+if [$x -gt $y];
 then
     echo "$x is less than $y"
-elif [$x < $y] 
+elif [$x -lt $y]; 
 then
     echo "$x is greater than $y"
 fi
 echo x=$x and y=$y
-: << comment  #anything after << will be taken as comments,: is called a null command
-if [$x -lt $y]
-then
-echo "$x is less than $y"
-else    
-echo "$x is greater than $y"
-fi
-echo x=$x and y=$y
+# : << comment  #anything after << will be taken as comments,: is called a null command
+# if [$x -lt $y]
+# then
+# echo "$x is less than $y"
+# else    
+# echo "$x is greater than $y"
+# fi
+# echo x=$x and y=$y
---
+++ tempCodeRunnerFile.sh
@@ -0,0 +1,8 @@
+: << comment  #anything after << will be taken as comments,: is called a null command
+# if [$x -lt $y]
+# then
+# echo "$x is less than $y"
+# else    
+# echo "$x is greater than $y"
+# fi
+# echo x=$x and y=$y
